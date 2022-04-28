1cb83a55d667d3a6b01a7c82b396fe31c0236a46
c0lares <mcolaresc@gmail.com>
2022-04-27 22:36:23 -0300

    feat: adicionando a navegacao na pagina

--- HTML/index.php
+++ HTML/index.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Loja</title>
+    <link rel="shortcut icon" href="../assets/imagens/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <h1>salve<h1>
-    
+    <header>
+        <nav class="navegacao">
+                <a href=""><span class="categorias">NOVIDADES</span></a>
+                <a href=""><span class="categorias">MASCULINO</span></a>
+                <a href=""><span class="categorias">FEMININO</span></a>
+                <a href=""><span class="categorias">CAMISETAS</span></a>
+                <a href=""><span class="categorias">BERMUDAS</span></a>
+        </nav>
+        <div class="icone">
+            <a href="#">
+                <img src="../assets/imagens/Vector 1.png" alt="">
+            </a>
+        </div>
+        <div class="pesquisa">
+            <form action="" method="get">
+                <input type="pesquisa" nome="pesquisa" id="pesquisa">
+            </form>
+        </div>
+        <div class="lupa">
+            <a href=""><img src="../assets/imagens/pngwing.com.png" alt=""></a>
+        </div>
+    </header>
 </body>
 </html>
